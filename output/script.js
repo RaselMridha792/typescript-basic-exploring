632a970617fb78bd3bb1b7f99952b6ae5f7bc8a5
RaselMridha792 <raselmridha792@gmail.com>
2025-02-17 02:44:59 +0600

    understand the simple types of typescript number, string, array, object etc

--- output/script.js
+++ output/script.js
@@ -1,3 +1,18 @@
 "use strict";
-const country = 'country';
-console.log(country);
+// let playerName = 'rasel';
+// playerName = 44;
+// console.log(playerName)
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+// console.log(multiply(2, 5));
+// let fruits = ["apple", true, "banana", false, 45, "orange"];
+// fruits.push(34);
+// console.log(fruits);
+// let person = {
+//       name: 'rasel',
+//       age: 21,
+//       isMarraid: false,
+// }
+// console.log(person.name = 42) // it is wrong
+// console.log(person.name = 'russell') // it is right
